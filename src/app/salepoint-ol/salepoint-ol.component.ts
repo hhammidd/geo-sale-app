867600a2f5124ef107b686c0781d9a2cce9ab96e
hamid <hamid.shafikani@gmail.com>
2021-12-22 06:37:19 +0100

    Initial commit

--- src/app/salepoint-ol/salepoint-ol.component.ts
+++ src/app/salepoint-ol/salepoint-ol.component.ts
@@ -1,8 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import Tile from "ol/layer/Tile";
-import OSM from "ol/source/OSM";
 import View from "ol/View";
 import MAP from "ol/Map";
+import Point from "ol/geom/Point";
+
+// @ts-ignore
+import OSM from 'ol/source/OSM';
+import TileWMS from 'ol/source/TileWMS';
+import TileLayer from "ol/layer/Tile";
+
+const place = [37.41, 8.82];
+const point = new Point(place);
+
 
 @Component({
   selector: 'app-salepoint-ol',
@@ -12,6 +21,7 @@ import MAP from "ol/Map";
 export class SalepointOlComponent implements OnInit {
 
   map;
+  wms;
   constructor() { }
 
   ngOnInit(): void {
@@ -24,10 +34,33 @@ export class SalepointOlComponent implements OnInit {
       layers: [
         new Tile({
           source: new OSM()
-        })
+        }),
+        new TileLayer({
+          source: new OSM(),
+        }),
+        new TileLayer({
+          extent: [-13884991, 2870341, -7455066, 6338219],
+          source: new TileWMS({
+            url: 'https://ahocevar.com/geoserver/wms',
+            params: {'LAYERS': 'topp:states', 'TILED': true},
+            serverType: 'geoserver',
+            // Countries have transparency, so do not fade tiles:
+            transition: 0,
+          }),
+        }),
+        // new TileLayer({
+        //   extent: [-13884991, 2870341, -7455066, 6338219],
+        //   source: new TileWMS({
+        //     url: 'http://localhost:8081/geoserver/wms',
+        //     params: {'LAYERS': 'tiger:giant_polygon', 'TILED': true},
+        //     serverType: 'geoserver',
+        //     // Countries have transparency, so do not fade tiles:
+        //     transition: 0,
+        //   }),
+        // }),
       ],
       view: new View({
-        center: [37.41, 8.82],
+        center: [-10997148, 4569099],
         zoom: 4
       })
     });
