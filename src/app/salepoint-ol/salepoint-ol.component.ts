a8ae162007a3415d23b2234aaa015a4586188f1e
hamid <hamid.shafikani@gmail.com>
2021-12-25 03:35:01 +0100

    select the map region

--- src/app/salepoint-ol/salepoint-ol.component.ts
+++ src/app/salepoint-ol/salepoint-ol.component.ts
@@ -40,79 +40,96 @@ export class SalepointOlComponent implements OnInit {
 
   initializeMap() {
 
-    const style = new Style({
-      fill: new Fill({
-        color: 'rgba(255, 255, 255, 0.6)',
-      }),
+    // const style = new Style({
+    //   fill: new Fill({
+    //     color: 'rgba(255, 255, 255, 0.6)',
+    //   }),
+    //   stroke: new Stroke({
+    //     color: '#319FD3',
+    //     width: 1,
+    //   }),
+    //   text: new Text({
+    //     font: '12px Calibri,sans-serif',
+    //     fill: new Fill({
+    //       color: '#000',
+    //     }),
+    //     stroke: new Stroke({
+    //       color: '#fff',
+    //       width: 3,
+    //     }),
+    //   }),
+    // });
+
+    // const vectorSource = new VectorSource({
+    //   format: new GeoJSON(),
+    //   url: function (extent) {
+    //     return (
+    //       'http://94.130.228.242:8082/geoserver/wfs?request=GetFeature&version=1.1.0&typeName=topp:states&formatName=GML2&FILTER=%3Cogc:Filter%20xmlns:ogc=%22http://www.opengis.net/ogc%22%3E%3Cogc:PropertyIsGreaterThan%3E%3Cogc:Div%3E%3Cogc:PropertyName%3EMANUAL%3C/ogc:PropertyName%3E%3Cogc:PropertyName%3EWORKERS%3C/ogc:PropertyName%3E%3C/ogc:Div%3E%3Cogc:Literal%3E0.25%3C/ogc:Literal%3E%3C/ogc:PropertyIsGreaterThan%3E%3C/ogc:Filter%3E' +
+    //       extent.join(',') +
+    //       ',EPSG:3857'
+    //     );
+    //   },
+    //   strategy: bboxStrategy,
+    // });
+    //
+    // const vector = new VectorLayer({
+    //   source: vectorSource,
+    //
+    //   style: new Style({
+    //     stroke: new Stroke({
+    //       color: 'rgba(0, 0, 255, 1.0)',
+    //       width: 2,
+    //     }),
+    //   }),
+    // });
+
+    // const regionBoarder = new TileLayer({
+    //   source: new TileWMS({
+    //     url: 'http://localhost:8082/geoserver/geosale/wms',
+    //     params: {'LAYERS': 'geosale:ITA_adm1', 'TILED': true},
+    //     // params: {'LAYERS': 'geosale:gadm36_NLD_2', 'TILED': true},
+    //     serverType: 'geoserver',
+    //     // transition: 0,
+    //   }),
+    //   opacity: 0.5
+    // });
+
+    const country = new Style({
       stroke: new Stroke({
-        color: '#319FD3',
+        color: 'gray',
         width: 1,
       }),
-      text: new Text({
-        font: '12px Calibri,sans-serif',
-        fill: new Fill({
-          color: '#000',
-        }),
-        stroke: new Stroke({
-          color: '#fff',
-          width: 3,
-        }),
+      fill: new Fill({
+        color: 'rgba(20,20,20,0.9)',
       }),
     });
 
-    const vectorSource = new VectorSource({
-      format: new GeoJSON(),
-      url: function (extent) {
-        return (
-          'http://94.130.228.242:8082/geoserver/wfs?request=GetFeature&version=1.1.0&typeName=topp:states&formatName=GML2&FILTER=%3Cogc:Filter%20xmlns:ogc=%22http://www.opengis.net/ogc%22%3E%3Cogc:PropertyIsGreaterThan%3E%3Cogc:Div%3E%3Cogc:PropertyName%3EMANUAL%3C/ogc:PropertyName%3E%3Cogc:PropertyName%3EWORKERS%3C/ogc:PropertyName%3E%3C/ogc:Div%3E%3Cogc:Literal%3E0.25%3C/ogc:Literal%3E%3C/ogc:PropertyIsGreaterThan%3E%3C/ogc:Filter%3E' +
-          extent.join(',') +
-          ',EPSG:3857'
-        );
-      },
-      strategy: bboxStrategy,
-
+    const selectedCountry = new Style({
+      stroke: new Stroke({
+        color: 'rgba(200,20,20,0.8)',
+        width: 2,
+      }),
+      fill: new Fill({
+        color: 'rgba(200,20,20,0.4)',
+      }),
     });
+
     //
     const vectorLayer = new VectorLayer({
       source: new VectorSource({
-         url: 'http://localhost:8082/geoserver/geosale/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geosale%3AITA_adm1&maxFeatures=50&outputFormat=application%2Fjson',
+        url: 'http://localhost:8082/geoserver/geosale/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geosale%3AITA_adm1&maxFeatures=50&outputFormat=application%2Fjson',
         // url: 'https://openlayers.org/en/v5.1.3/examples/data/geojson/countries.geojson', //world
         // url: 'http://localhost:8081/geoserver/geosale/wms?service=WMS&version=1.1.0&request=GetMap&layers=geosale%3Agadm36_NLD_2&bbox=3.3607819080352783%2C50.72349166870117%2C7.227095127105656%2C53.55458450317383&width=768&height=562&srs=EPSG%3A4326&styles=&format=application/openlayers',
         format: new GeoJSON(),
       }),
-      style: function (feature) {
-        style.getText().setText(feature.get('name'));
-        return style;
-      },
+      style: country
     });
-    //
-    //
+
     const baseMap = new Tile({source: new OSM()});
-    const regionBoarder = new TileLayer({
-      source: new TileWMS({
-        url: 'http://localhost:8082/geoserver/geosale/wms',
-         params: {'LAYERS': 'geosale:ITA_adm1', 'TILED': true},
-        // params: {'LAYERS': 'geosale:gadm36_NLD_2', 'TILED': true},
-        serverType: 'geoserver',
-        // transition: 0,
-      }),
-      opacity: 0.5
-    });
-    //
-    const vector = new VectorLayer({
-      source: vectorSource,
-
-      style: new Style({
-        stroke: new Stroke({
-          color: 'rgba(0, 0, 255, 1.0)',
-          width: 2,
-        }),
-      }),
-    });
-
     const grp = new LayerGroup({
       layers: [baseMap, vectorLayer],
     });
+    //
 
     const map = new MAP({
       target: 'map',
@@ -120,26 +137,55 @@ export class SalepointOlComponent implements OnInit {
       view: new View({
         // projection: 'EPSG:900913',
         center: [-78906677.036667, 5444438.895],
-        zoom: 4
+        zoom: 5
       }),
     });
+    // Selection
+    const selectionLayer = new VectorLayer({
+      map: map,
+      // renderMode: 'vector',
+      source: vectorLayer.getSource(),
+      style: function (feature) {
+        if (feature.getId()! in selection) {
+          return selectedCountry;
+        }
+      },
+    });
+    //
+
+    let selection = {};
 
     const overlayContainerElement: HTMLElement = document.querySelector('.overlay-container')!;
     const overlayLayer = new Overlay({
       element: overlayContainerElement
-    })
+    });
     map.addOverlay(overlayLayer);
     const overLayFeatureId = document.getElementById('feature-id')!;
 
 
     map.on('click', function (e) {
-      map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
-        let clickedCoordinate = e.coordinate;
-        let clickedFeatureId : any = feature.getId()
-        overlayLayer.setPosition(clickedCoordinate);
-        overLayFeatureId.innerHTML = clickedFeatureId;
-
-        console.log(clickedFeatureId);// get the codes
+      // map.forEachFeatureAtPixel(e.pixel, function (feature, layer) {
+      //   let clickedCoordinate = e.coordinate;
+      //   let clickedFeatureId : any = feature.getId();
+      //   overlayLayer.setPosition(clickedCoordinate);
+      //   overLayFeatureId.innerHTML = clickedFeatureId;
+      //
+      //   console.log(clickedFeatureId);// get the codes
+      // })
+      vectorLayer.getFeatures(e.pixel).then(function(features) {
+        if (!features.length) {
+          selection = {};
+          selectionLayer.changed();
+          return;
+        }
+        const feature = features[0];
+        if (!feature) {
+          return;
+        }
+        const fid : any = feature.getId();
+        selection[fid] = feature;
+        selectionLayer.changed();
+        console.log(fid);
       })
     })
     // map.on('click', function (e) {
